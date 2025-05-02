4fe4a3b21247dab0ac419a818a203d650e2cb516
KusalG05 <96484725+KusalG05@users.noreply.github.com>
2025-05-02 07:39:17 +0530

    Update eval.py

--- Toxicity/eval.py
+++ Toxicity/eval.py
@@ -1,39 +1,12 @@
-# This file evaluates responses saved in BBQ_{category}.json
-
 import json
-import argparse
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--category', type=str, help='Bias Category - Religion or Gender_identity or Age')
-args = parser.parse_args()
+with open('english_Toxic_scores.json','r') as f:
+    english_scores = json.load(f)
 
-category = args.category
-dic = {
-    "A" : 0,
-    "B" : 1,
-    "C" : 2
-}
+score = 0
 
+for i in range(len(english_scores)):
+    score = score + english_scores[i]['Continuation Toxicity']
 
-with open(f'BBQ_{category}.json', 'r') as f:
-    data = json.load(f)
-    
-ambig_count = 0
-ambig_correct = 0
-
-disambig_count = 0
-disambig_correct = 0
-
-for i in data:
-    if i["context_condition"] == "disambig":
-        disambig_count += 1
-        if dic[i["model_ans"]] == i["correct_ans"]:
-            disambig_correct += 1
-    else:
-        ambig_count += 1
-        if dic[i["model_ans"]] == i["correct_ans"]:
-            ambig_correct += 1
-            
-print(f"Accuracies for category {category} in ambiguous case is {round(ambig_correct/ambig_count, 4)} and Disambiguous case is {round(disambig_correct/disambig_count, 4)}")
-
-print(ambig_correct, ambig_count, disambig_correct, disambig_count)
+score = score / len(english_scores)
+print("Average Toxicity Score: ", score)
